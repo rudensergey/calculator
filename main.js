8567a675d279ab97f41a58ae87065dcb4ee35956
rudensergey <rudensergey@gmail.com>
2020-04-21 05:05:59 +0300

    add smartphone gestures

--- main.js
+++ main.js
@@ -7,6 +7,53 @@ let current = document.getElementById("term__current");
 
 let days = 20;
 
+
+// #####################################
+
+toggle.ontouchstart = function (event) {
+    event.preventDefault(); // предотвратить запуск выделения (действие браузера)
+    
+
+    var touchLoc = event.targetTouches[0];
+    let shiftX = touchLoc.clientX - toggle.getBoundingClientRect().left;
+    // shiftY здесь не нужен, слайдер двигается только по горизонтали
+
+    document.addEventListener("touchmove", onMouseMove);
+    document.addEventListener("touchend", onMouseUp);
+
+    function onMouseMove(e) {
+        let touchLocation = e.targetTouches[0];
+
+        let newLeft =
+            touchLocation.clientX - shiftX - timeline.getBoundingClientRect().left;
+
+        let coords = timeline.getBoundingClientRect();
+
+        let point =
+            (coords.left + coords.width - 18 - (coords.left + 36)) / 345;
+        days =
+            Math.round((touchLocation.clientX - (coords.left + 36)) / point) + 20;
+
+        showFinalSum();
+
+        // курсор вышел из слайдера => оставить бегунок в его границах.
+        if (newLeft < 0) newLeft = 0;
+        let rightEdge = timeline.offsetWidth - toggle.offsetWidth;
+        if (newLeft > rightEdge) newLeft = rightEdge;
+
+        toggle.style.left = newLeft + "px";
+    }
+
+    function onMouseUp() {
+        document.removeEventListener("touchmove", onMouseUp);
+        document.removeEventListener("touchend", onMouseMove);
+    }
+};
+
+
+// ##############################################
+
+
 toggle.onmousedown = function (event) {
     event.preventDefault(); // предотвратить запуск выделения (действие браузера)
 
@@ -61,8 +108,8 @@ layout.addEventListener("click", (e) => {
 });
 
 function showFinalSum() {
-    if (days <= 20) days = 20;
-    if (days >= 365) days = 365;
+    if (days < 20) days = 20;
+    if (days > 365) days = 365;
     let currency = 1;
     let deposit = input.value;
 
